b76fd3102fafbc182977b47e1fc6079754b28388
iamSamandar <yusupov1031@gmail.com>
2020-09-20 08:54:11 +0500

    add search button and filter

--- js/js/main.js
+++ js/js/main.js
@@ -1,3 +1,19 @@
+let selected = document.querySelector(".selected");
+let optionsContainer = document.querySelector(".options-container")
+
+let optionList = document.querySelectorAll(".options")
+
+selected.addEventListener("click", () => {
+    optionsContainer.classList.toggle("active")
+});
+optionList.forEach((o) => {
+    o.addEventListener("click", () => {
+        selected.innerHTML = o.querySelector("label").innerHTML;
+        optionsContainer.classList.remove("active")
+    })
+}) 
+ 
+ 
  AOS.init({
  	duration: 800,
  	easing: 'slide'
